3f0b26956b6a70a6e54fdf35dda3c89ea11a81a9
kevin-bonnot <bonnot.kevin@gmail.com>
2023-05-04 17:11:31 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Kévin Bonnot</h1>
+      <h2>Fullstack developer</h2>
+      <h3>Mes projets</h3>
+      <a href="https://what2watch.kevinbonnot.fr">What2Watch</a>
+      <a href="https://3d-text.kevinbonnot.fr">Démo three.js</a>
+    </>
+  )
+}
+
+export default App
