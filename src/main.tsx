40e9903ec2a89b55398d4f8e02969e82b8b275a3
Kévin Bonnot <bonnot.kevin@gmail.com>
2023-08-02 00:20:45 +0200

    Add github links + theming

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import '@fontsource/Abril-Fatface/400.css';
+import '@fontsource/abril-fatface/400.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
